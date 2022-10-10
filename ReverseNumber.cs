89f6016d1947e877a734ff739b42d6841cb65847
yashdondal <yashdondal21@gmail.com>
2022-10-10 08:09:42 +0530

    Get a Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblemDay6
+{
+    internal class ReverseNumber
+    {
+        public static void findReverseNumber()
+        {
+            int n;
+            int reverse = 0;
+            int rem;
+            Console.WriteLine("Please enter the number-");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reverse No is {0}", reverse);
+        }
+    }
+}
