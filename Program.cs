c920487bd492ae1b6293dadf06d1579725c2081c
yashdondal <yashdondal21@gmail.com>
2022-10-10 07:49:30 +0530

    Logical Problem

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LogicalProblemDay6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // CouponCode.DistinctCouponNumber();
+            //FibonacciSeries.DisplayFibonacciSeries();
+            //PerfectNumber.CheckPerfectnumber();
+            //PrimeNumber.CheckprimeNumber();
+            ReverseNumber.findReverseNumber();
+        }
+    }
+}
